35d9a4144b1459abc14aad65d0a2bc7864f786ee
Dmitry <ds-8.5@bk.ru>
2023-03-02 20:31:38 +0300

    fix: промежуточное исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,31 @@
+let namePerson = document.querySelector('.profile__nike-name');
+let discriptionPerson = document.querySelector('.profile__discription');
 let editButton = document.querySelector('.profile__edit-btn');
 let form = document.querySelector('.popup__form');
 let nameInput = form.querySelector('.popup__input-name');
 let discriptionInput = form.querySelector('.popup__input-discription');
 let closeButton = document.querySelector('.popup__btn-close');
-let setLikeButton = document.querySelector('.element__like');
+let popup = document.querySelector('.popup');
 
 form.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openEditProfile);
 closeButton.addEventListener('click', closeEditProfile);
 
 function openEditProfile() {
-    let popup = document.querySelector('.popup');
     popup.classList.toggle('popup_opened');
 
-    let name = document.querySelector('.profile__nike-name');
-    let discription = document.querySelector('.profile__discription');
-
-
-    nameInput.value = name.textContent;
-    discriptionInput.value = discription.textContent;
+    nameInput.value = namePerson.textContent;
+    discriptionInput.value = discriptionPerson.textContent;
 }
 
 function closeEditProfile() {
-    let popup = document.querySelector('.popup');
     popup.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
-    let name = document.querySelector('.profile__nike-name');
-    let discription = document.querySelector('.profile__discription');
 
-    name.textContent = nameInput.value;
-    discription.textContent = discriptionInput.value;
+    namePerson.textContent = nameInput.value;
+    discriptionPerson.textContent = discriptionInput.value;
     closeEditProfile();
 }
