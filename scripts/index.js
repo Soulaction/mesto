0296c398225a1de13e069da19a73f977ffc9c8d9
Dmitry <ds-8.5@bk.ru>
2023-03-03 18:48:09 +0300

    fix: исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ let nameUser = document.querySelector('.profile__nike-name');
 let discriptionUser = document.querySelector('.profile__discription');
 let editButton = document.querySelector('.profile__edit-btn');
 let form = document.querySelector('.popup__form');
-let nameInput = form.querySelector('.popup__form_type_name');
-let discriptionInput = form.querySelector('.popup__form_type_discription');
+let nameInput = form.querySelector('.popup__input_type_name');
+let discriptionInput = form.querySelector('.popup__input_type_discription');
 let closeButton = document.querySelector('.popup__btn-close');
 let popup = document.querySelector('.popup');
 
